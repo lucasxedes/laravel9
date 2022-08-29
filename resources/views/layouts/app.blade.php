625f4cf07262abff3a4ae242088e3c774d001d02
lucasxedes <luk_288@hotmail.com>
2022-08-29 13:58:38 -0300

    Authentication / Registration / Recover Password

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,15 +4,21 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="shortcut icon" href="{{ url('favicon.ico') }}" type="png">
-    <script src="https://cdn.tailwindcss.com"></script>
     <title>@yield('title') - EspecializaTi</title>
+
+    <link rel="shortcut icon" href="{{ url('images/favicon.ico') }}" type="image/png">
+
+    <script src="https://cdn.tailwindcss.com"></script>
 </head>
 <body class="bg-gray-50">
-    
-     <div class="container mx-auto px-4 py-8">
-            @yield('content')
-     </div>
+
+    <div class="container mx-auto px-4 py-8">
+        <form action="{{ route('logout') }}" method="post">
+            @csrf
+            <button type="submit" class="shadow bg-red-500 hover:bg-red-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded">Logout</button>
+        </form>
+        @yield('content')
+    </div>
 
 </body>
 </html>
